883624b5c52c292adcf049ec9926768999446b34
giovannamoeller <giovannagmoeller@gmail.com>
2024-11-24 19:14:49 -0300

    Initial Commit

---
+++ CultAI/CultAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultAIApp.swift
+//  CultAI
+//
+//  Created by Giovanna Moeller on 24/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CultAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CultAITests/CultAITests.swift
@@ -0,0 +1,17 @@
+//
+//  CultAITests.swift
+//  CultAITests
+//
+//  Created by Giovanna Moeller on 24/11/24.
+//
+
+import Testing
+@testable import CultAI
+
+struct CultAITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
